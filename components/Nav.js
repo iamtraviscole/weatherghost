6a75dddbdf134a421e8858d05f8d1301e2e42193
Travis Cole <iamtraviscole@gmail.com>
2021-09-14 23:06:23 -0400

    add logo

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 
+import Logo from '../public/weatherghostlogo.svg'
+
 export default function Nav() {
   return (
     <nav className='Nav'>
       <div className='Nav__logo'>
-        <Link href='/'><a>logo here</a></Link>
+        <Link href='/'><a><Logo /></a></Link>
       </div>
       <ul className='Nav__links'>
         <li><Link href='/'><a>Lorem</a></Link></li>
