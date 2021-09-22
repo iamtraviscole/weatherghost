d0203bfba4b86ae7898c166ceb5117009762a763
Travis Cole <iamtraviscole@gmail.com>
2021-09-22 03:38:46 -0400

    add color and font, style inputs and buttons

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,10 @@ class MyDocument extends Document {
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-          <link href='https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap' rel='stylesheet' />
+          <link 
+            href='https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Montserat:wght@300;400;700&display=swap' 
+            rel='stylesheet' 
+          />
         </Head>
         <body>
           <Main />
