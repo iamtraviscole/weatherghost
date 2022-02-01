0a50ef53228bad61129535a6a7cdc618ad770624
Travis Cole <iamtraviscole@gmail.com>
2022-02-01 03:43:53 -0500

    add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
           <link 
